93c65a7828388968e5e9a8cd08c760776ec30a5d
Sunny  Prakash <sunny.pprakash@gmail.com>
2021-05-31 18:02:57 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 3000
+const port = 3000;
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // your code goes here
 
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.post("/add", (req, res) => {
+    console.log(req.body);
+    let resObj = {
+        status: "success",
+        message: "the sum of given two numbers",
+        sum: req.body.num1 + req.body.num2,
+    };
+    res.send(resObj);
+});
+app.post("/sub", (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+app.post("/multiply", (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+app.post("/divide", (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
